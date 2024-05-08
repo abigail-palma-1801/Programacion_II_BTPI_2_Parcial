257fe85d363031a5bf58ebfb091f4fd0d6655c82
Mayrin Abigail <mayrinpalma200719@gmail.com>
2024-05-08 14:30:13 -0600

    clase 08/05/2024

---
+++ funciones_2.py
@@ -0,0 +1,13 @@
+
+
+
+def sumar(numero1:int, numero2: int) -> int:
+    #suma = numero1 + numero2
+    #return suma
+
+    return numero1 + numero2
+
+
+resultado = sumar(4,2)
+print(resultado)
+print(sumar(4,4))
