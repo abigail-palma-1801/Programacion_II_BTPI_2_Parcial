b3b244cf580913b2ee2f843ecb6e14223b3d62a5
Mayrin Abigail <mayrinpalma200719@gmail.com>
2024-05-13 14:31:07 -0600

    clase del 13/05/2024

--- funciones_2.py
+++ funciones_2.py
@@ -8,6 +8,24 @@ def sumar(numero1:int, numero2: int) -> int:
     return numero1 + numero2
 
 
-resultado = sumar(4,2)
-print(resultado)
+#resultado = sumar(4,2)
+#print(resultado)
 print(sumar(4,4))
+
+
+def restar(numero1:int, numero2:int) -> int:
+    return  numero1 - numero2
+
+print(restar(8,2))
+
+
+def mutiplicacion(numero1:int, numero2:int) -> int:
+    return numero1 * numero2
+
+print(mutiplicacion(5,2))
+
+
+def dividir(numero1:int, numero2:int) -> int:
+    return numero1 / numero2
+
+print(dividir(20,5))
