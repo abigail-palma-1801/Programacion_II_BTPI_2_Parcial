8904d54587c1e83598e32a6b9333a0fee08c23b7
Mayrin Abigail <mayrinpalma200719@gmail.com>
2024-07-09 14:19:53 -0600

    clase del 9/7/2024

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,9 +1,8 @@
 from menu import imprimir_menu
-from datos import crear_tarea,obtener_todas_las_tareas
-import json
+from datos import crear_tarea,obtener_todas_las_tareas, eliminar_tarea, obtener_tareas_pendientes, obtener_tareas_completadas, completar_tarea
+#import json
 
-salir = True
-
+salir = False
 
 #with open('test.json') as testF:
  #   texto = testF.read()
@@ -14,21 +13,21 @@ salir = True
  #   print(data['usuarios'])
 
 
-test_data= {
-    'texto': 'Hola Mundo',
-    'estado': True,
-    'valor': 3.1
-}
-
-print(test_data)
-
-json_text = json.dumps(test_data)
-print(json_text)
-
-with open('datos.json','w') as datos_file:
-    datos_file.write(json_text)
-    datos_file.close()
-
+#test_data= {
+#    'texto': 'Hola Mundo',
+#    'estado': True,
+#    'valor': 3.1
+#}
+#
+#print(test_data)
+#
+#json_text = json.dumps(test_data)
+#print(json_text)
+#
+#with open('datos.json','w') as datos_file:
+#    datos_file.write(json_text)
+#    datos_file.close()
+#si se utiliza open sin ningun argumento, este leera en modo lectura
 
 while not salir:
     imprimir_menu()
@@ -43,14 +42,52 @@ while not salir:
             index = Tareas.index(Tarea)
             print(f'{index + 1}. {Tarea['titulo']}')
     elif resp == '2':
-        pass
+         print('\n------Tareas Pendientes------')
+         Tareas = obtener_tareas_pendientes()
+         for Tarea in Tareas:
+            index = Tareas.index(Tarea)
+            print(f'{index + 1}. {Tarea['titulo']}')
     elif resp == '3':
-        pass
+         print('\n------Tareas Completadas------')
+         Tareas = obtener_tareas_completadas()
+         for Tarea in Tareas:
+            index = Tareas.index(Tarea)
+            print(f'{index + 1}. {Tarea['titulo']}')
     elif resp == '4':
         titulo_tarea = input('ingrese la nueva tarea: ')
         crear_tarea(titulo_tarea)
         print('\nTarea Agregada!\n\n')
     elif resp == '5':
-        pass
+        print('\n------ Todas las Tareas------')
+        Tareas = obtener_todas_las_tareas()
+        for Tarea in Tareas:
+            index = Tareas.index(Tarea)
+            print(f'{index + 1}. {Tarea['titulo']}') 
+        del_opt = input("Ingrese el numero de la tarea que desea eliminar: ")
+
+        del_index = int(del_opt) - 1
+
+        del_tarea = Tareas[del_index]
+
+        eliminar_tarea(del_tarea)
+
+        print('\nTarea Eliminada!\n\n')
+    
+    elif resp == '6':
+        print('\n----- Completar una tarea -----')
+        tareas = obtener_tareas_pendientes()
+        for tarea in tareas:
+            index = tareas.index(tarea)
+            print(f'{index + 1}, {tarea['titulo']}')
+        
+        comp_opt = input("ingrese el numero de la tarea que desea completar: ")
+
+        comp_index = int(comp_opt) - 1
+
+        comp_tarea = tareas[comp_index]
+        
+        completar_tarea(comp_tarea['id'])
+
+        print('\nTarea Completada!\n\n')
     else:
         print(f"la opcion '{resp}' no es valida ")
